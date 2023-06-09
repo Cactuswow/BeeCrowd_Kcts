90e73df8537b206a2c08fa2dde891adfbece7cc8
Kactuswow <bdalvarezg@ufpso.edu.co>
2023-06-09 14:04:28 -0500

    Crear secuencia Fibonnaci Inicialización del proyecto

--- Web Designs/AngularJS/AngularTailwindCSS/src/app/app.module.ts
+++ Web Designs/AngularJS/AngularTailwindCSS/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponentComponent } from './components/card-component/card-component.component';
+import { FibonacciComponent } from './components/fibonacci/fibonacci.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponentComponent
+    CardComponentComponent,
+    FibonacciComponent
   ],
   imports: [
     BrowserModule,
